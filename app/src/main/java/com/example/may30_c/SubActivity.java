f7cd958fd75ba33546cdd3c3dd113e1a53cccdf1
hnslzy <hnslzy@gmail.com>
2017-06-05 20:03:07 +0900

    3. EditText와 TextView를 이용한 간단한 입출력
    참고)
    do it 안드로이드 앱 프로그래밍,
    https://medium.com/android-develop-android/android%EA%B0%9C%EB%B0%9C-4-edittext%EC%99%80-textview%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EA%B0%84%EB%8B%A8%ED%95%9C-%EC%9E%85%EC%B6%9C%EB%A0%A5-b212dea7740a

--- app/src/main/java/com/example/may30_c/SubActivity.java
+++ app/src/main/java/com/example/may30_c/SubActivity.java
@@ -2,6 +2,10 @@ package com.example.may30_c;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+
+//        Button을 눌렀을 때 EditText에 들어간 텍스트를 TextView로 보냄, Button에 리스너 생성
+        final EditText edittext=(EditText)findViewById(R.id.edittext);
+        Button button=(Button)findViewById(R.id.button);
+        final TextView textView=(TextView)findViewById(R.id.textview);
+
+//      button은 클릭했을 때 액션을 취함함 -> setnClickListener를 이용, 리스너 생성.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // onClick메소드 안에 버튼이 클릭되었을 때 실행될 행동 삽입.
+            public void onClick(View v) {
+                // 버튼이 눌렸을 때 나올 액션 설정
+                // textView.setText();는 TextView에 들어갈 텍스트를 설정하는 코드
+                // edittext.getText()는 EditText에서 입력된 텍스트를 불러오는 코드
+                textView.setText(edittext.getText());
+            }
+        });
     }
 }
+
+
